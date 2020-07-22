8651b25bd482ac834036d5998ae88788efc4567c
mlpierce22 <mlpierce22@gmail.com>
2020-07-22 12:26:10 -0700

    clean up component and make it so that it still works with shadow dom encapsulation, and recompile with new changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,43 +8,60 @@ import {
   SimpleChanges,
   ChangeDetectorRef,
   ViewEncapsulation,
+  ViewChild,
+  ElementRef,
 } from "@angular/core";
 import { Subject } from "rxjs";
 import Hls from "hls.js";
-import { LiveStream, VideoDimensions } from './app.models';
+import { LiveStream, VideoDimensions } from "./app.models";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
-  encapsulation: ViewEncapsulation.ShadowDom
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"],
+  encapsulation: ViewEncapsulation.ShadowDom,
 })
 export class AppComponent implements OnInit, OnChanges {
-
+  /** Represents the stream object with all it's metadata. */
   @Input() stream: LiveStream;
 
+  /** Constantly updated with the dimension of the stream video based on window size. */
   @Input() streamDim: VideoDimensions;
 
+  /** The image to display instead of the livestream. (if this is null, display stream) */
   @Input() posterUrl: string;
 
+  /** When the user clicks on the video. */
   @Output() focusVideo: EventEmitter<void> = new EventEmitter<void>();
 
+  /** When the user hovers over the video. */
   @Output() showBorder: EventEmitter<boolean> = new EventEmitter<boolean>();
+
+  /** When the video errors either because too many retries or link not working. */
+  @Output() hasErrored: EventEmitter<void> = new EventEmitter<void>();
+
+  /** The video tag so we don't have to worry about shadow doms and encapsulation */
+  @ViewChild("stream", { static: false }) videoElement: ElementRef;
 
   constructor(public cdr: ChangeDetectorRef) {}
 
-  ngOnInit() {
-  }
+  ngOnInit() {}
 
   ngOnChanges(changes: SimpleChanges): void {
     //Called before any other lifecycle hook. Use it to inject dependencies, but avoid any serious work here.
 
     // In the future, we may have to decide if this is the desired behavior
-    if (!!changes["stream"] && this.posterUrl == null && changes["stream"].firstChange) {
+    if (
+      !!changes["stream"] &&
+      this.posterUrl == null &&
+      changes["stream"].firstChange
+    ) {
       this.setupHls(this.stream);
     }
     if (
-      !!changes["stream"] && this.posterUrl == null && !!changes["stream"].previousValue &&
+      !!changes["stream"] &&
+      this.posterUrl == null &&
+      !!changes["stream"].previousValue &&
       changes["stream"].currentValue["manifestUrl"] !==
         changes["stream"].previousValue["manifestUrl"]
     ) {
@@ -55,13 +72,14 @@ export class AppComponent implements OnInit, OnChanges {
     }
   }
 
+  /** Setup the HLS stream */
   setupHls(stream: LiveStream) {
-    let exists = setInterval(function () {
-      if (stream && document.querySelector("camio-live-streams").shadowRoot.getElementById("live-stream-" + stream.id)) {
+    let retryCount = 3;
+    // Retry Interval every 100ms
+    let exists = setInterval(() => {
+      let camioStream = this.videoElement.nativeElement;
+      if (stream && retryCount != 0 && camioStream) {
         clearInterval(exists);
-        let video = document.querySelector("camio-live-streams").shadowRoot.getElementById(
-          "live-stream-" + stream.id
-        ) as HTMLVideoElement;
         if (Hls.isSupported()) {
           let hls = new Hls();
           let retries = { network: 0, media: 0 };
@@ -81,24 +99,30 @@ export class AppComponent implements OnInit, OnChanges {
                 default:
                   // cannot recover
                   hls.destroy();
+                  this.hasErrored.emit()
                   break;
               }
             }
           });
 
-          hls.attachMedia(video);
+          hls.attachMedia(camioStream);
           hls.on(Hls.Events.MEDIA_ATTACHED, () => {
-            video.muted = true;
-            video.controls = true;
-            video.loop = true;
+            camioStream.muted = true;
+            camioStream.controls = true;
+            camioStream.loop = true;
             hls.loadSource(stream.manifestUrl);
             hls.on(Hls.Events.MANIFEST_PARSED, (event, data) => {
               //console.log(data)
             });
           });
-        } else if (video.canPlayType("application/vnd.apple.mpegurl")) {
-          video.src = stream.manifestUrl;
+        } else if (camioStream.canPlayType("application/vnd.apple.mpegurl")) {
+          camioStream.src = stream.manifestUrl;
         }
+      } else if (retryCount == 0) {
+        clearInterval(exists);
+        this.hasErrored.emit();
+      } else {
+        retryCount--;
       }
     }, 100);
   }
